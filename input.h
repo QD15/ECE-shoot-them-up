a70a9f52e049689de49da6d5f25082df6f61ab78
QD15 <quentin.desnos@edu.ece.fr>
2026-04-23 22:43:31 +0200

    Update input.h

--- input.h
+++ input.h
@@ -3,11 +3,10 @@
 
 #include <allegro5/allegro.h>
 
-/* Variables globales : etat des touches (1 = enfoncee, 0 = relachee) */
 extern int key_up, key_down, key_left, key_right;
 extern int key_space, key_enter, key_escape;
 
-/* Met a jour les variables selon l'evenement recu */
+
 void input_handle_event(ALLEGRO_EVENT *ev);
 
 #endif
