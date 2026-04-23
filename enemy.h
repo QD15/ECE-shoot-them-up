329fb554ca3c24162dff83f5dd3388ae32f6ef5e
QD15 <quentin.desnos@edu.ece.fr>
2026-04-23 22:40:14 +0200

    Refactor enemy structure and update comments

--- enemy.h
+++ enemy.h
@@ -3,26 +3,23 @@
 
 #include <allegro5/allegro.h>
 
-/* Dimensions et nombre max d'ennemis */
 #define ENEMY_W           50
 #define ENEMY_H           50
 #define MAX_ENNEMIS       8
 #define MAX_BALLES_ENE    16  /* balles tirees par les ennemis */
 
-/* Une balle ennemie : se deplace vers la gauche */
 typedef struct {
     float x, y;
     int active;
 } BalleEnnemie;
 
-/* Un ennemi : position, drapeau actif, compteur de tir */
 typedef struct {
     float x, y;
     int active;
-    int timer_tir;  /* decremente chaque frame, tire quand il atteint 0 */
+    int timer_tir;  
 } Enemy;
 
-/* Tableau global des balles ennemies (gere dans enemy.c) */
+
 extern BalleEnnemie balles_ene[MAX_BALLES_ENE];
 
 void ennemis_init(Enemy ennemis[]);
