87a46cc21bf7a15939d8f13be8c5f9c8d77222b3
oscar-pwls <oscarpauwels@gmail.com>
2026-05-13 08:39:09 +0200

    Add additional key variables for input handling

--- input.h
+++ input.h
@@ -3,10 +3,13 @@
 
 #include <allegro5/allegro.h>
 
+/* Variables globales : etat des touches (1 = enfoncee, 0 = relachee) */
 extern int key_up, key_down, key_left, key_right;
 extern int key_space, key_enter, key_escape;
+extern int key_1, key_2, key_3;   /* acces direct aux niveaux depuis le menu */
 
-
+/* Met a jour les variables selon l'evenement recu */
 void input_handle_event(ALLEGRO_EVENT *ev);
 
 #endif
+
