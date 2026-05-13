5bdcc2b3a5805078a051b8ce1302d21edaffa442
oscar-pwls <oscarpauwels@gmail.com>
2026-05-13 08:39:31 +0200

    Add key handling for keys 1, 2, and 3

--- input.c
+++ input.c
@@ -1,15 +1,16 @@
 #include <allegro5/allegro.h>
 #include "input.h"
 
+/* Etat de chaque touche */
 int key_up = 0, key_down = 0, key_left = 0, key_right = 0;
 int key_space = 0, key_enter = 0, key_escape = 0;
+int key_1 = 0, key_2 = 0, key_3 = 0;
 
 void input_handle_event(ALLEGRO_EVENT *ev) {
     int val;
 
-   
-    if (ev->type == ALLEGRO_EVENT_KEY_DOWN) val = 1;
-    else if (ev->type == ALLEGRO_EVENT_KEY_UP) val = 0;
+    if (ev->type == ALLEGRO_EVENT_KEY_DOWN)      val = 1;
+    else if (ev->type == ALLEGRO_EVENT_KEY_UP)   val = 0;
     else return;
 
     switch (ev->keyboard.keycode) {
@@ -20,5 +21,8 @@ void input_handle_event(ALLEGRO_EVENT *ev) {
         case ALLEGRO_KEY_SPACE:  key_space  = val; break;
         case ALLEGRO_KEY_ENTER:  key_enter  = val; break;
         case ALLEGRO_KEY_ESCAPE: key_escape = val; break;
+        case ALLEGRO_KEY_1:      key_1      = val; break;
+        case ALLEGRO_KEY_2:      key_2      = val; break;
+        case ALLEGRO_KEY_3:      key_3      = val; break;
     }
 }
